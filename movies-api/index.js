fbfeea6dfac65cac95d6e6288a5c82d882b15f34
Elizabeth Neary <20071724@mail.wit.ie>
2023-11-30 15:47:10 +0000

    Added Movies Route

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,8 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import usersRouter from './api/users';
-import './db1';
+import './db';
 import defaultErrHandler from './errHandler'
+import moviesRouter from './api/movies';   //import movies router
+
 
 dotenv.config();
 
@@ -12,6 +14,7 @@ const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
 app.use('/api/users', usersRouter);
 app.use(defaultErrHandler);
 
