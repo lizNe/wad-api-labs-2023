935ddb077b9c7b4770ec984b58db7aaa3c84eefa
Elizabeth Neary <20071724@mail.wit.ie>
2023-12-05 15:23:23 +0000

    Topic 12 - React App setup

---
+++ reactApp/src/api/movies-api.js
@@ -0,0 +1,6 @@
+export const getMovies = async () => {
+    const response = await  fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=285eb1b0be19f5fbb9e0ac3de08089ae&language=en-US&include_adult=false&page=1`
+    )
+    return response.json()
+  };
